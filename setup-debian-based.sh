f3512ec459a6ba18180fa4c9a821e461b7562e98
Paul Ritterbush <ritterbush@berkeley.edu>
2021-12-20 14:20:30 -0800

    Removed redundant dependency

--- setup-debian-based.sh
+++ setup-debian-based.sh
@@ -30,7 +30,7 @@ fi # end of -c option is not used
 # Nothing with running as $username seems to be working like I want, so just temporarily change permissions for executing/writing to new user's home folder:
 
 cat > /home/${username}/new-user-setup.sh <<End-of-message
-(echo "$password"; echo; echo; echo) | sudo -S apt install xorg xinit zsh git firefox feh sxiv imagemagick fonts-linuxlibertine neofetch htop mpd ncmpcpp suckless-tools asciidoc libxinerama-dev libxft2-dev libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson cmake python3 ninja-build libpcre3 libpcre3-dev python3-pip pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev
+(echo "$password"; echo; echo; echo) | sudo -S apt install xorg xinit zsh git firefox feh sxiv imagemagick fonts-linuxlibertine neofetch htop mpd ncmpcpp suckless-tools asciidoc libxinerama-dev libxft2-dev libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson cmake python3 ninja-build libpcre3 python3-pip pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev
 
 
 # Install Alacritty by adding a PPA repository
